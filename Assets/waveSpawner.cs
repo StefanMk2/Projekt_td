4ee6aead38f76ea33497f9c0e70529d410fc2a7a
Mantoir <michal1.pietraszewski@gmail.com>
2025-03-13 20:30:44 +0100

    koniec 3

--- Assets/waveSpawner.cs
+++ Assets/waveSpawner.cs
@@ -1,35 +1,42 @@
 using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
 
 public class WaveSpawner : MonoBehaviour {
 
 	public Transform enemyPrefab;
 	public Transform spawnPoint;
+
 	public float timeBetweenWaves = 5f;
 	private float countdown = 2f;
 
-	private int waveNumber = 1;
+	public Text waveCountdownText;
+
+	private int waveIndex = 0;
 
 	void Update ()
 	{
 		if (countdown <= 0f)
 		{
-			SpawnWave();
+			StartCoroutine(SpawnWave());
 			countdown = timeBetweenWaves;
 		}
 
 		countdown -= Time.deltaTime;
 
+		waveCountdownText.text = Mathf.Round(countdown).ToString();
+
 	}
 
-	void SpawnWave ()
+	IEnumerator SpawnWave ()
 	{
+		waveIndex++;
 
-		for (int i = 0; i < waveNumber; i++)
+		for (int i = 0; i < waveIndex; i++)
 		{
 			SpawnEnemy();
+			yield return new WaitForSeconds(0.5f);
 		}
-
-		waveNumber++;
 	}
 
 	void SpawnEnemy ()
