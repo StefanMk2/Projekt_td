5a692ea19bb3e37a8a1a710ba31c98600fdaa557
JakubN16 <narog16080@nauka.panschelm.edu.pl>
2025-05-14 15:34:18 +0200

    13 update
    Zmieniłem sposób wyświetlania ilości żyć

--- Assets/script/LivesUI.cs
+++ Assets/script/LivesUI.cs
@@ -11,6 +11,6 @@ public class LivesUI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        livesText.text = PlayerStats.Lives + " LIVES";
+        livesText.text = "Życia:" + PlayerStats.Lives;
     }
 }
