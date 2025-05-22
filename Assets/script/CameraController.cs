b7e2c3bcac14d3671bccfab4341b4b0a87f27baf
JakubN16 <narog16080@nauka.panschelm.edu.pl>
2025-05-22 14:31:38 +0200

    21 pause menu

--- Assets/script/CameraController.cs
+++ Assets/script/CameraController.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
 
-    private bool doMovement = true;
  
     public float panSpeed = 30f;
     public float scrollSpeed = 5f;
@@ -18,12 +17,6 @@ public class CameraController : MonoBehaviour
             this.enabled = false;
             return;
         }
-
-        if (Input.GetKeyDown(KeyCode.Escape))
-        doMovement = !doMovement;
-
-        if (!doMovement)
-            return;
 
         if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
         {
