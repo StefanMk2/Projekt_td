d88e87c7d7833064a394f680e5512f7dac9d5430
Mantoir <michal1.pietraszewski@gmail.com>
2025-05-29 01:37:29 +0200

    naprawiłem błąd BUILD

--- Assets/script/Waypoints.cs
+++ Assets/script/Waypoints.cs
@@ -1,16 +1,19 @@
 using UnityEngine;
 
 public class Waypoints : MonoBehaviour {
+    public static Transform[] points;
 
-	public static Transform[] points;
+    void Awake() {
+        if (points == null || points.Length == 0) {
+            points = new Transform[transform.childCount];
+            for (int i = 0; i < points.Length; i++) {
+                points[i] = transform.GetChild(i);
+            }
+        }
+    }
 
-	void Awake ()
-	{
-		points = new Transform[transform.childCount];
-		for (int i = 0; i < points.Length; i++)
-		{
-			points[i] = transform.GetChild(i);
-		}
-	}
-
+    // Add this to ensure initialization
+    void OnEnable() {
+        Awake();
+    }
 }
