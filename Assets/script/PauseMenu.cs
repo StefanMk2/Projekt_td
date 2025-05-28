d2179ade0b911a56561dd48da493ddd43950676c
JakubN16 <narog16080@nauka.panschelm.edu.pl>
2025-05-28 12:07:01 +0200

    24 animacje przejść między scenami

--- Assets/script/PauseMenu.cs
+++ Assets/script/PauseMenu.cs
@@ -4,6 +4,8 @@ using UnityEngine.SceneManagement;
 public class PauseMenu : MonoBehaviour
 {
     public GameObject ui;
+    
+    public SceneFader sceneFader;
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.P))
@@ -28,11 +30,12 @@ public class PauseMenu : MonoBehaviour
     public void Retry()
     {
         Toggle();
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        sceneFader.FadeTo(SceneManager.GetActiveScene().name);
     }
 
     public void Menu()
     {
-        Debug.Log("Go to menu.");
+        Toggle();
+        sceneFader.FadeTo("MainMenu");
     }
 }
